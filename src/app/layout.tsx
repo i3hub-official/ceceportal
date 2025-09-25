58485ee10994b7e04dbd321ab323a5b9329eb647
OgwoGP <digispowerllc@gmail.com>
2025-09-25 19:37:01 +0100

    Updated packages and pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
 
 import { HeaderProvider } from "@/contexts/HeaderContext";
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "CEC Portal - eRegistration",
@@ -46,7 +36,10 @@ export default function RootLayout({
       </head>
       <body
         suppressHydrationWarning
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className="antialiased"
+        style={{
+          fontFamily: "var(--font-sans)",
+        }}
       >
         <HeaderProvider>
           <main className="pt-15">{children}</main>
