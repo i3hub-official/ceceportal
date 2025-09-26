e6515d4f411af259d4b75bd4d1ccf7d0caca523b
OgwoGP <digispowerllc@gmail.com>
2025-09-27 00:45:51 +0100

    Remove unused pages

--- src/lib/utils/auths/index.ts
+++
@@ -1,6 +0,0 @@
-// File: src/emails/auths/index.ts
-export { ForgotPasswordEmail } from "./forgot-password";
-export { PasswordResetEmail } from "./password-reset";
-export { PasswordChangedEmail } from "./password-changed";
-export { VerifyEmailEmail } from "./verify-email";
-export { SignInNotificationEmail } from "./signin-notification";
